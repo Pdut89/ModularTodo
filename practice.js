b8ffe04aefa96e40052cde8f3a162d945f4b9e29
Pdut89 <pdutoit@fullfacing.com>
2017-12-21 16:31:00 +0200

    add public methods

--- practice.js
+++ practice.js
@@ -1,63 +1,70 @@
 console.log("hello from practice.js");
 
-$(document).ready(function(){
 
-  var todos = (function(){
 
-    var todos = ['clean', 'cook'];
+var todos = (function(){
 
-    // Keeps cache of dom elements
-    // cacheDom
-    var $deleteBtn = $('.btn-danger');
-    var $addBtn = $('.add-item');
-    var $list = $('.list-group');
-    var $input = $('input');
+  var todos = ['clean', 'cook'];
 
-    // bindEvents
-    $addBtn.on('click', addTodo);
-    $list.delegate('.list-item .delete', 'click', deleteTodo);
+  // Keeps cache of dom elements
+  // cache dom
+  var $deleteBtn = $('.btn-danger');
+  var $addBtn = $('.add-item');
+  var $list = $('.list-group');
+  var $input = $('input');
 
-    _buildList();
-    // Render the initial list of items
-    function _buildList(){
-      todos.forEach(insertItem);
+  // bind events
+  $addBtn.on('click', addTodo);
+  $list.delegate('.list-item .delete', 'click', deleteTodo);
+
+  _buildList();
+  // Render the initial list of items
+  function _buildList(){
+    todos.forEach(insertItem);
+  }
+
+  // Reusable - inserts todo items
+  function insertItem(e){
+    if (e) {
+      $list.append(itemStructure(e));
     }
+  }
 
-    // Reusable - inserts todo items
-    function insertItem(e){
-      if (e) {
-        $list.append(itemStructure(e));
-      }
+  // Reusable html snippet
+  function itemStructure(e){
+    return (
+      '<div class="list-item">' +
+        '<li class="list-group-item">' + e + '</li>' +
+        '<button type="button"class="delete btn btn-danger">X</button>' +
+      '</div>')
+  }
+
+  // Adds input value to todos array & todo array
+  function addTodo(value){
+    var item = (typeof value === "string") ? value : $input.val();
+    if (item) {
+      todos.push(item);
+      insertItem(item);
     }
+    $input.val('');
+  }
 
-    // Reusable html snippet
-    function itemStructure(e){
-      return (
-        '<div class="list-item">' +
-          '<li class="list-group-item">' + e + '</li>' +
-          '<button type="button"class="delete btn btn-danger">X</button>' +
-        '</div>')
-    }
+  // Removes specific item from html & todos array
+  function deleteTodo(){
+    $unwanted = $(event.target).parent('.list-item');
+    var i = $list.find('.list-item').index($unwanted);
 
-    // Adds input value to todos array & todo array
-    function addTodo(){
-      item = $input.val();
-      if (item) {
-        todos.push(item);
-        insertItem(item);
-      }
-      $input.val('');
-    }
+    todos.splice(i, 1);
+    $unwanted.remove();
+  }
 
-    // Removes specific item from html & todos array
-    function deleteTodo(){
-      $unwanted = $(event.target).parent('.list-item');
-      var i = $list.find('.list-item').index($unwanted);
+  function hello(){
+    console.log("hi")
+  }
 
-      todos.splice(i, 1);
-      $unwanted.remove();
-    }
+  return {
+    hello: hello,
+    addTodo: addTodo
+  }
 
-  })();
-
-});
+})();
